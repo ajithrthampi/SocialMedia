061681010836e8576d8defc92673e7794bff014c
phozpho <ajithrthampi999@gmail.com>
2023-03-04 21:43:18 +0530

    Updated package.json with build

--- socket/index.js
+++ socket/index.js
@@ -52,7 +52,7 @@ io.on("connection", (socket) => {
     })
   }) 
       
-              
+            
   socket.on("disconnect",()=>{
     console.log('a user disconnected');
     removeUser(socket.id)           
