8383d2c9a018c326782b54491846ea4e058b1648
phozpho <ajithrthampi999@gmail.com>
2023-03-07 17:28:03 +0530

    profileSkeleton

--- my-app/src/skeleton/ProfileMobileSkeleton.tsx
+++ my-app/src/skeleton/ProfileMobileSkeleton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProfileMobileSkeleton = () => {
     return (
         <>
-            <div className="md:hidden border border-[#3b3838] shadow rounded-md p-4  w-screen ">
+            <div className="md:hidden border border-[#3b3838] shadow rounded-md p-4  w-screen h-screen ">
                 <div className='flex flex-row gap-10 items-center'>
                     <div className="rounded-full bg-[#3b3838] h-20 w-20 "></div>
                     <div className="h-2 bg-[#3b3838] rounded w-16 col-span-2"></div>
